2725c4d84edf8c3389455648e9a6796aedae47a9
saliveja <esman@riseup.net>
2022-05-08 12:13:58 +0200

    nested sequence converted into multidimensional array

--- chapter_4/creating_ndarrays.py
+++ chapter_4/creating_ndarrays.py
@@ -4,3 +4,12 @@ import numpy as np
 data1 = [6, 7.5, 8, 0, 1]
 arr1 = np.array(data1)
 print(arr1)
+print('\n')
+
+data2 = [[1, 2, 3, 4], [5, 6, 7, 8]]
+# nested sequence converted into multidimensional array
+arr2 = np.array(data2)
+print(arr2)
+print('\n')
+print(arr2.ndim)
+# returns 2. lists stacked on top of each other.

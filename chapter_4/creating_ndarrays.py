d58559e9a666f07c87f4fc10daf7fbb32cdd2be4
saliveja <esman@riseup.net>
2022-05-08 12:33:59 +0200

    using numpy zeros and empty

--- chapter_4/creating_ndarrays.py
+++ chapter_4/creating_ndarrays.py
@@ -14,7 +14,7 @@ print('\n')
 print(arr2.ndim)
 # returns 2. lists stacked on top of each other.
 print(arr2.shape)
-# the shape of the array is four numbers in two lines.
+# the shape of the array is four numbers in two rows.
 print('\n')
 
 print(arr1.dtype)
@@ -27,4 +27,14 @@ print(arr1.dtype)
 # interpreted as a value that is undefined or unrepresentable.
 print(arr2.dtype)
 # int64 is for storing whole numbers
-
+print('\n')
+print(np.zeros(10))
+# np.zeros creates arrays of 0 or 1s with given length or shape
+# prints ten zeros
+print(np.zeros((3, 6)))
+# note the double brackets
+# prints three rows of six zeros in each
+print(np.empty((2, 3, 2)))
+# np.empty creates an array without initializing any particular value
+# np.empty don't necessarily print only zeros
+# it may return uninitialized garbage values

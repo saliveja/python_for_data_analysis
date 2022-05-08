299a00ca7f95de4673e509d00a2a88445ebfad38
saliveja <esman@riseup.net>
2022-05-08 12:02:29 +0200

    data shape a dtype

--- chapter_4/multidimensional_array_object.py
+++ chapter_4/multidimensional_array_object.py
@@ -13,3 +13,12 @@ print('\n')
 # adding the randomly generated data on top of each other
 data = data + data
 print(data)
+print('\n')
+
+# in ndarray all elements need to be the same type
+print(data.shape)
+# every array has a shape, which is a tuple indicating the size of each
+# dimension
+print('\n')
+print(data.dtype)
+# dtype is an object describing the data type  of the array

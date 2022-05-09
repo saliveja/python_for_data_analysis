1deeae7788be69c969346c8b9d3aa8cb62faf2b0
saliveja <esman@riseup.net>
2022-05-09 09:43:25 +0200

    arrays instead of for loops, all elements addressed

---
+++ chapter_4/arithmetic_with_numpy_arrays.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+arr = np.array([[1., 2., 3.], [4., 5., 6.]])
+# pay attention to the number of brackets
+# when using arrays we don't have to use for loops
+print(arr)
+print(arr * arr)
+print(arr - arr)
+print(1 / arr)
+print(arr ** 0.5)
+# whatever calculation, all elements are addressed

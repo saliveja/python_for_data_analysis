79dd914d98e744d8db3c29d854d75b1406e4a7f2
saliveja <esman@riseup.net>
2022-05-09 09:46:19 +0200

    boolean arrays

--- chapter_4/arithmetic_with_numpy_arrays.py
+++ chapter_4/arithmetic_with_numpy_arrays.py
@@ -9,3 +9,8 @@ print(arr - arr)
 print(1 / arr)
 print(arr ** 0.5)
 # whatever calculation, all elements are addressed
+
+# boolean arrays
+arr2 = np.array([[0., 4., 1.], [7., 2., 12]])
+print(arr2)
+print(arr2 > arr)

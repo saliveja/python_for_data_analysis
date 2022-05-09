d3bdce53329a067e7e4e55e2ec6b2032b66c431d
saliveja <esman@riseup.net>
2022-05-09 10:02:46 +0200

    modifying arrays

--- chapter_4/basic_indexing_and_slicing.py
+++ chapter_4/basic_indexing_and_slicing.py
@@ -8,5 +8,17 @@ print(arr[5])
 # modification will be reflected in the source
 print(arr[5:8])
 arr[5:8] = 12
-# replacing the numbers 5, 6 and 8 with the number 12
+# assigning value 12 to numbers 5, 6 and 7
+print(arr)
+
+# modifying arrays
+arr_slice = arr[5:8]
+print(arr_slice)
+arr_slice[1] = 12345
+# assigning value 12345 to index 1 in the slice
+# we can slice the array and slice the slice
+print(arr)
+arr_slice[:] = 64
+# assigning value 64 to all values in the slice
+# the modifications are permanent. NumPy makes no copies.
 print(arr)

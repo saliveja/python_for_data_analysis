dc5637f6c4077ede86c945c4ac6cd226482ad54e
saliveja <esman@riseup.net>
2022-05-09 11:04:24 +0200

    passing multiple indexes

--- chapter_4/indexing_with_slices.py
+++ chapter_4/indexing_with_slices.py
@@ -11,3 +11,9 @@ arr2d = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 print(arr2d[:2])
 # slicing works differently with two-dimensional arrays
 # this means --> select the two first rows of arr2d
+
+# passing multiple indexes
+print(arr2d[:2, 1:])
+# this means to slice the first two rows
+# from index one until the end
+# in this case, two numbers will be printed for each row

e5534fea4951815286f39550f11ecdccd8b83b0b
saliveja <esman@riseup.net>
2022-05-09 11:01:13 +0200

    slicing one-dimensional and tw-dimensional arrays

---
+++ chapter_4/indexing_with_slices.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# slicing one-dimensional arrays
+arr = np.arange(10)
+# print(arr[1:6])
+# one-dimensional slicing works like with python lists
+
+# slicing two-dimensional arrays
+arr2d = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+# print(arr2d)
+print(arr2d[:2])
+# slicing works differently with two-dimensional arrays
+# this means --> select the two first rows of arr2d

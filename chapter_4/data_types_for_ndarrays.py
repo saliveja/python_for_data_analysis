7b37d7f24a4b9823603a0e9c7cae13c5b946ce26
saliveja <esman@riseup.net>
2022-05-09 09:33:22 +0200

    using another arrays dtype attribute

--- chapter_4/data_types_for_ndarrays.py
+++ chapter_4/data_types_for_ndarrays.py
@@ -26,4 +26,9 @@ print(arr.astype(np.int32))
 numeric_strings = np.array(['1.25', '-9.6', '42'], dtype=np.string_)
 print(numeric_strings.astype(float))
 
+# using another arrays dtype attribute
+int_array = np.arange(10)
+calibers = np.array([.22, .270, .357, .380, .44, .50], dtype=np.float64)
+print(int_array.astype(calibers.dtype))
+
 

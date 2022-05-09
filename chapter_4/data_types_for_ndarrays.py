8f6608e5ecfaf8696d2b2964152aacc3d75255e4
saliveja <esman@riseup.net>
2022-05-09 09:24:06 +0200

    casting float to int, truncating decimals

--- chapter_4/data_types_for_ndarrays.py
+++ chapter_4/data_types_for_ndarrays.py
@@ -1,14 +1,25 @@
 import numpy as np
 
+# specified dtype
 arr1 = np.array([1, 2, 3], dtype=np.float64)
 arr2 = np.array([1, 2, 3], dtype=np.int32)
 print(arr1.dtype)
 print(arr2.dtype)
-# dtype is specified in arr1 and arr2
+
+# casting int/float
 arr = np.array([1, 2, 3, 4, 5])
 print(arr.dtype)
 # dtype is int64
 float_arr = arr.astype(np.float64)
 # casting integers to floating points
+# if floating points are cast to integers the decimal part will be truncated
 print(float_arr.dtype)
 # float64
+
+# casting float to int, truncating decimals
+arr = np.array([3.7, -1.2, -2.6, 0.5, 12.9, 10.1])
+print(arr)
+# printing the sequence as shown
+print(arr.astype(np.int32))
+
+

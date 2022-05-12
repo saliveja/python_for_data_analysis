2e8b2485ecf2173470b8905a54b529ce36f57d0f
saliveja <esman@riseup.net>
2022-05-12 10:52:03 +0200

    adding notes on np.dot

--- chapter_4/transposing_arrays_and_swapping_axes.py
+++ chapter_4/transposing_arrays_and_swapping_axes.py
@@ -13,3 +13,13 @@ print(arr.T)
 # this displays the columns in the order of rows
 # and the rows in the order of columns
 # [[0, 5, 10]] instead of [[0, 1, 2]]
+print("\n")
+
+# np.dot
+arr = np.random.randn(6, 3)
+print(arr)
+print("\n")
+
+print(np.dot(arr.T, arr))
+# this mean row 1, index  0 * row 2, index 0 + row 2, index 1 + row 2, index 2
+# it continues in the same fashion with all number addressing the row below

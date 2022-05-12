b9fdf8670bfbd5b3246f4ae2900e3151af907e93
saliveja <esman@riseup.net>
2022-05-12 11:49:07 +0200

    multiple arrays with unfunc

--- chapter_4/4.2/universal_functions.py
+++ chapter_4/4.2/universal_functions.py
@@ -24,7 +24,19 @@ print("\n")
 
 print(np.maximum(x, y))
 # returns the biggest numbers from x and y into a single array
+print("\n")
 
+# multiple arrays with unfunc
+arr = np.random.randn(7) * 5
+print(arr)
+print("\n")
+
+remainder, whole_part = np.modf(arr)
+# modf return the fractional and integral parts of an array
+# for example:np.modf([0, 3.5]), will return:
+# (array([ 0. ,  0.5]), array([ 0.,  3.]))
+print(remainder)
+print(whole_part)
 
 
 

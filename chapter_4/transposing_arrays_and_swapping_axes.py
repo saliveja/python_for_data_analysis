378fe45761bfa8240a430a81094181b28e76e6f1
saliveja <esman@riseup.net>
2022-05-12 09:49:58 +0200

    arr.T

--- chapter_4/transposing_arrays_and_swapping_axes.py
+++ chapter_4/transposing_arrays_and_swapping_axes.py
@@ -7,3 +7,9 @@ arr = np.arange(15).reshape((3, 5))
 # five columns
 # this method doesn't copy anything
 print(arr)
+print("\n")
+
+print(arr.T)
+# this displays the columns in the order of rows
+# and the rows in the order of columns
+# [[0, 5, 10]] instead of [[0, 1, 2]]
